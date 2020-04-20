6bf3bcd0c57d9171ec33d79c1ced6efb4ddc288c
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-20 15:15:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,12 +3,11 @@
 def display_rainbow(colors)
   
   print "#{colors[0][0].upcase}: #{colors[0]},"
-  puts "#{colors[1][0].upcase}: #{colors[1]},"
-  puts "#{colors[2][0].upcase}: #{colors[2]},"
-  puts "#{colors[3][0].upcase}: #{colors[3]},"
-  puts "#{colors[4][0].upcase}: #{colors[4]},"
-  puts "#{colors[5][0].upcase}: #{colors[5]},"
-  puts "#{colors[6][0].upcase}: #{colors[6]}"
-  
+  print "#{colors[1][0].upcase}: #{colors[1]},"
+  print "#{colors[2][0].upcase}: #{colors[2]},"
+  print "#{colors[3][0].upcase}: #{colors[3]},"
+  print "#{colors[4][0].upcase}: #{colors[4]},"
+  print "#{colors[5][0].upcase}: #{colors[5]},"
+  print "#{colors[6][0].upcase}: #{colors[6]}"
   
 end
