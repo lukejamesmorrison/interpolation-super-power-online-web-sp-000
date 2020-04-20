2207fc40090aeccb6dc6f4d346c639847745576d
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-20 15:11:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  
+  output = ''
+  
+  colors.each |color| do
+    
+    color_text = "#{color[0].upcase}: #{color},"
+    
+    output = output + color_text
+    
+  end
+  
+end
