9b97ada4482008d13f90e9cb3e1d999947f49426
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-20 15:14:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,11 @@
 def display_rainbow(colors)
   
   puts "#{color[0][0].upcase}: #{color[0]},"
+  puts "#{color[1][0].upcase}: #{color[1]},"
+  puts "#{color[2][0].upcase}: #{color[2]},"
+  puts "#{color[3][0].upcase}: #{color[3]},"
+  puts "#{color[4][0].upcase}: #{color[4]},"
+  puts "#{color[5][0].upcase}: #{color[5]},"
+  
   
 end
